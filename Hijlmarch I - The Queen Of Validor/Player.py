7837bd1d09fa6216591cc9107d93c6bfdc5d8322
Window <walkerrob302@gmail.com>
2021-11-03 13:27:53 -0700

    Missing document from previous commits.
    Documentation that was missing from last commits is now in player.py

--- Hijlmarch I - The Queen Of Validor/Player.py
+++ Hijlmarch I - The Queen Of Validor/Player.py
@@ -1,6 +1,20 @@
 class Player:
 
-    """ This class contains everything required for the player to function. """
+    """ This class contains everything required for the player to function. 
+        
+        :param health: The amount of health that the player has remaining.
+        :param inventory: The items that the player is holding onto.
+        :param equipped_item: The item the player is holding onto.
+        
+        :type health: Integer
+        :type inventory: List
+        :type equipped_item: Item
+
+        Example
+        >> non_existing_class = Item()
+        >> player_obj = Player(100, [], non_exisiting_class)
+    """
+
 
     def __init__(self, health, inventory, equipped_item):
         self.health = health
@@ -9,21 +23,39 @@ class Player:
 
     
     def add_item_to_inventory(self, item):
-        """ Adds an item to the Player object inventory. """
+        """ Adds an item to the Player object inventory.
+            
+            :param item: The item in question.
+
+            :type item: Item
+        """
         self.inventory.append(item)
 
     
     def equip_item(self, item):
-        """ Equips an item for the player. """
+        """ Equips an item for the player. 
+
+            :param item: The item that the player wants to equip.
+
+            :type item: Item
+        """
         self.equipped_item = item
 
     
     def increase_health(self, amount_to_add_health):
-        """ Increases the health of the player. """
+        """ Increases the health of the player by amount_to_add_health.
+
+            :type amount_to_add_health: Integer
+        """
         self.health += amount_to_add_health
     
     
-    def decrease_health(self, amount_to_sub_health):
-        """ Decrease the health of the player. """
+    def decrease_health(self, amount_to_subtract_health):
+        """ Decrease the health of the player by amount_to_subtract_health.
+
+            :type amount_to_subtract_health: Integer 
+        """
         self.health -= amount_to_sub_health
+
+
 
